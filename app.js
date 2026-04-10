a75ed11aa5820e26380836542da4c5e69a515391
Krish <krish@example.com>
2026-04-10 21:50:56 +0530

    Fix: Add login page and fix route path

--- app.js
+++ app.js
@@ -283,7 +283,7 @@ app.get('/simple-test', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 app.get('/complete-profile', (req, res) => {
